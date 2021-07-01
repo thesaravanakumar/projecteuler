59baf9c32d4ea78cd7d264e706b9c7712735b63e
Saravana Kumar <saravana.k.manimaran@gmail.com>
2021-07-01 15:04:59 +0530

    16th one

---
+++ 16-Powerdigitsum.cpp
@@ -0,0 +1,25 @@
+#include              <bits/stdc++.h>
+#define ll            long long int
+#define w(t)          int t; cin>>t; while(t--)
+#define F             first
+#define S             second
+#define pb            push_back
+#define mp            make_pair
+#define pii           pair<int,int>
+#define mii           map<int,int>
+#define sp(x,y)       fixed<<setprecision(y)<<x
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll n=pow(2,1000);
+    cout<<n;
+    ll sum=0;
+    while(n){
+        sum+=n%10;
+        n/=10;
+    }
+    cout<<sum;
+    return 0;
+}
